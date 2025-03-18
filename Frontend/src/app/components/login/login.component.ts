ecce68e0880c6f685e5b524bd75550dde5beed4e
MrBoqueron <fabiotudero@gmail.com>
2025-03-18 10:38:18 +0100

    Actualizar enlace de registro en el formulario de inicio de sesión y agregar RouterModule a las importaciones

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })

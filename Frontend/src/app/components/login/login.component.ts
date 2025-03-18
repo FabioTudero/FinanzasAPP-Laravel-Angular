9349c3cb80042c444d601c2a91a97308709c2b19
FabioTudero <fabiotudero@gmail.com>
2025-03-18 01:25:18 +0100

    Agregar formulario de inicio de sesión con validación y estilos

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  loginForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.loginForm = fb.group({
+      'name': [''],
+      'password': ['']
+    });
+  }
+
+  async onSubmit(value: any) {
+    console.log('Submitted value: ', value);
+  }
 
 }

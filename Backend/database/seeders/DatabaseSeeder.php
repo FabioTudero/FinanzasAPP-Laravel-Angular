c62f229d9cdec225dcbcb19d94daf11d2f514d93
Fabio Tudero <fabiotudero@gmail.com>
2025-03-21 13:18:43 +0100

    Refactor el seeder de categorías de transacciones para usar un array y un bucle

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -2,20 +2,28 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Seeder;
 use App\Models\CategoryTransaction;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         // User::factory(10)->create();
 
-        CategoryTransaction::create(['name' => 'Salario'], ['name' => 'Alquiler'], ['name' => 'Comida'], ['name' => 'Transporte'], ['name' => 'Entretenimiento'], ['name' => 'Invesion'], ['name' => 'Otros']);
+        $categories = [
+            'Salario',
+            'Alquiler',
+            'Comida',
+            'Transporte',
+            'Entretenimiento',
+            'Inversión',
+            'Otros'
+        ];
+
+        foreach ($categories as $category) {
+            CategoryTransaction::create(['name' => $category]);
+        }
     }
 }

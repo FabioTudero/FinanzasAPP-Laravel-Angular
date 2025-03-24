74ac457ec9eea6385368702a81ebf655c6ba70ab
FabioTudero <fabiotudero@gmail.com>
2025-03-24 22:38:41 +0100

    Corregir el tipo de transacción de 'IMCOME' a 'INCOME' en la interfaz de transacción

--- Frontend/src/app/interfaces/transaction.ts
+++ Frontend/src/app/interfaces/transaction.ts
@@ -2,7 +2,7 @@ export interface Transaction {
     id: number;
     category_transaction_id: number;
     amount: number;
-    type: 'IMCOME' | 'EXPRENSE';
+    type: 'INCOME' | 'EXPRENSE';
     description: string;
     date: string;
 }

1fe3d7787e36490d7f30799dab37be897567b8db
Fabio Tudero <fabiotudero@gmail.com>
2025-03-25 10:22:00 +0100

    Agregar la selección de meses en el componente del dashboard

--- Frontend/src/app/pages/dashboard/dashboard.component.ts
+++ Frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -6,6 +6,11 @@ import { Balance } from '../../interfaces/balance';
 import { CommonModule } from '@angular/common';
 import { Transaction } from '../../interfaces/transaction';
 
+interface Month {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -13,21 +18,36 @@ import { Transaction } from '../../interfaces/transaction';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-
-
 export class DashboardComponent {
   user?: User;
   currentMonth: string;
   balance: Balance | undefined;
   transactions: Transaction[] | undefined;
+  selectedMonth: Month | undefined;
+  months: Month[] = [
+    { id: 1, name: 'Enero' },
+    { id: 2, name: 'Febrero' },
+    { id: 3, name: 'Marzo' },
+    { id: 4, name: 'Abril' },
+    { id: 5, name: 'Mayo' },
+    { id: 6, name: 'Junio' },
+    { id: 7, name: 'Julio' },
+    { id: 8, name: 'Agosto' },
+    { id: 9, name: 'Septiembre' },
+    { id: 10, name: 'Octubre' },
+    { id: 11, name: 'Noviembre' },
+    { id: 12, name: 'Diciembre' }
+  ];
 
   constructor(private authService: AuthService, private transactionService: TransactionService) {
-    const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 
-      'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-    this.currentMonth = months[new Date().getMonth()];
+    const date = new Date();
+    this.currentMonth = this.months[date.getMonth()].name;
+    this.selectedMonth = this.months[date.getMonth()];
   }
 
   ngOnInit() {
+    console.log(this.months[new Date().getMonth()]);
+    
     this.authService.getUser().subscribe({
       next: (userData) => {
         this.user = userData;

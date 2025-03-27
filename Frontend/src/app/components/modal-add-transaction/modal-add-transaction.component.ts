d34abfe66fe731c76b278a3884fd89a069e43bf0
Mr_Boqueron <fabiotudero@gmail.com>
2025-03-27 22:30:57 +0100

    Agregar formulario reactivo para añadir transacciones y cargar categorías desde el servicio

--- Frontend/src/app/components/modal-add-transaction/modal-add-transaction.component.ts
+++ Frontend/src/app/components/modal-add-transaction/modal-add-transaction.component.ts
@@ -1,18 +1,37 @@
 import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
-
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Category } from '../../interfaces/category';
+import { TransactionService } from '../../services/transaction.service';
 
 @Component({
   selector: 'app-modal-add-transaction',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './modal-add-transaction.component.html',
   styleUrl: './modal-add-transaction.component.css'
 })
 export class ModalAddTransactionComponent {
 
+  addTransactionForm: FormGroup;
+  categories: Category[] | undefined;
+
   @Input() show = false;
   @Output() close = new EventEmitter<void>();
 
+  constructor(
+    private fb: FormBuilder,
+    private transactionService: TransactionService
+  ) {
+    this.addTransactionForm = this.fb.group({
+      category_transaction_id: [''],
+      amount: [''],
+      type: [''],
+      description: [''],
+      day: [''],
+      month: [''],
+      year: ['']
+    });
+  }
   // Detectar tecla "Esc" y cerrar el modal
   @HostListener('document:keydown.escape', ['$event'])
   onEscKeydown(event: KeyboardEvent) {
@@ -21,6 +40,17 @@ export class ModalAddTransactionComponent {
 
   closeModal() {
     this.close.emit();
+  }
+  
+  ngOnInit() {
+    this.transactionService.getCategories().subscribe({
+      next: (categories) => {
+        this.categories = categories;
+      },
+      error: (err) => {
+        console.error('Error fetching categories:', err);
+      }
+    });
   }
 
 }

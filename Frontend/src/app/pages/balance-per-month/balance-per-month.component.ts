b92fd3373104dd190004906dc745ef89380b14d8
Fabio Tudero <fabiotudero@gmail.com>
2025-03-27 13:01:21 +0100

    Implementar el componente 'BalancePerMonth' con funcionalidad para mostrar el balance y las transacciones mensuales

--- Frontend/src/app/pages/balance-per-month/balance-per-month.component.ts
+++ Frontend/src/app/pages/balance-per-month/balance-per-month.component.ts
@@ -1,12 +1,80 @@
 import { Component } from '@angular/core';
+import { TransactionService } from '../../services/transaction.service';
+import { Balance } from '../../interfaces/balance';
+import { BalanceCardComponent } from "../../components/balance-card/balance-card.component";
+import { BalanceListComponent } from "../../components/balance-list/balance-list.component";
+import { Transaction } from '../../interfaces/transaction';
 
 @Component({
   selector: 'app-balance-per-month',
   standalone: true,
-  imports: [],
+  imports: [BalanceCardComponent, BalanceListComponent],
   templateUrl: './balance-per-month.component.html',
   styleUrl: './balance-per-month.component.css'
 })
 export class BalancePerMonthComponent {
 
+  months: any[] | undefined;
+  currentMonth: number;
+  currentYear: number;
+  balance: Balance | undefined;
+  transactions: Transaction[] | undefined;
+
+  constructor(private transactionService: TransactionService) {
+    this.currentMonth = new Date().getMonth() + 1;
+    this.currentYear = new Date().getFullYear();
+    this.transactionService.getMonths().subscribe({
+      next: (months) => {
+        this.months = months;
+      },
+      error: (err) => {
+        console.error('Error fetching months:', err);
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.getBalance();
+    this.getTransactions();
+  }
+    
+  getMonth(id: number) {
+    this.currentMonth = id;
+    this.getBalance();
+    this.getTransactions();
+  }
+
+  sumYear() {
+    this.currentYear++;
+    this.getBalance();
+    this.getTransactions();
+  }
+  
+  subtractYear() {
+    this.currentYear--;
+    this.getBalance();
+    this.getTransactions();
+  }
+
+  getBalance() {
+    this.transactionService.getBalance(this.currentMonth, this.currentYear).subscribe({
+      next: (balance) => {
+        this.balance = balance;
+      },
+      error: (err) => {
+        console.error('Error fetching balance:', err);
+      }
+    });
+  }
+
+  getTransactions() {
+    this.transactionService.getTransactions(this.currentMonth, this.currentYear).subscribe({
+      next: (transactions) => {
+        this.transactions = transactions;
+      },
+      error: (err) => {
+        console.error('Error fetching transactions:', err);
+      }
+    });
+  }
 }

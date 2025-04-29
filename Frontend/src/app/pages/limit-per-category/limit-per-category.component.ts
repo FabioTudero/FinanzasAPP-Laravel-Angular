1652f10d1ce0bac8fb0b1fb2946a1313d46dca83
Fabio Tudero <fabiotudero@gmail.com>
2025-04-29 11:15:46 +0200

    Agregar formulario para establecer límites por categoría y manejar la lógica de envío

--- Frontend/src/app/pages/limit-per-category/limit-per-category.component.ts
+++ Frontend/src/app/pages/limit-per-category/limit-per-category.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { Category } from '../../interfaces/category';
+import { TransactionService } from '../../services/transaction.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-limit-per-category',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './limit-per-category.component.html',
   styleUrl: './limit-per-category.component.css'
 })
 export class LimitPerCategoryComponent {
+
+  addLimitFormm: FormGroup;
+  categories: Category[] | undefined;
+
+  constructor(
+    private fb: FormBuilder,
+    private transactionService: TransactionService
+  ){
+    this.transactionService.getCategories().subscribe({
+      next: (categories) => {
+        this.categories = categories;
+      },
+      error: (err) => {
+        console.error('Error fetching categories:', err);
+      }
+    });
+    this.addLimitFormm = this.fb.group({
+      category_transaction_id: [''],
+      limit: [''],
+    });
+  }
+
+  onSubmit() {
+    this.transactionService.addLimit(this.addLimitFormm.value).subscribe({
+      next: (response) => {
+        console.log('Limit added:', response);
+      },
+      error: (err) => {
+        console.error('Error adding limit:', err);
+      }
+    });
+  }
 
 }
